b161dbd8e7a7d4515aeb307cd193b1bb36c61099
BernadaPope <popebernada@.com>
2023-05-07 19:18:12 -0400

    updates

--- app.js
+++ app.js
@@ -1,2 +1,7 @@
 const body = document.body;
 const title = document.querySelector("donuts");
+
+body.addEventListener("click", function (event) {
+    if (event.target.classList.contains("page-title"))
+    console.log    
+});
