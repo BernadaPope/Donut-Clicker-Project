a0eed069037f8c44ef05f2f684ba9ff2603cef82
BernadaPope <popebernada@.com>
2023-05-12 21:39:48 -0400

    updates

--- app.js
+++ app.js
@@ -11,22 +11,48 @@ button.addEventListener("click", function () {
 const autoClickerSection = document.getElementById("autoClickerSection");
 const autoClickerCount = document.getElementById("autoClickerCount");
 const purchaseButton = document.getElementById("purchaseButton");
+const autoClickerCost = document.getElementById("autoClickerCost");
 
 let ClickerCount = 0;
 let donutsCount = 15;
+let clickerCost = 20;
 
 function updateButtonState() {
   if (donutsCount >= 15) {
     purchaseButton.disabled = false;
-    purchaseButton.style.backgroundColor = "";
+    purchaseButton.style.backgroundColor = '';
   } else {
     purchaseButton.disabled = true;
-    purchaseButton.style.backgroundColor = "gray";
+    purchaseButton.style.backgroundColor = 'gray';
   }
 }
 
-purchaseButton.addEventListener("click", function () {
-  ClickerCount++;
+function updateButtonState() {
+    if (donutsCount >= clickerCost) {
+        purchaseButton.disabled = false;
+        purchaseButton.style.backgroundColor = '';
+    } else {
+        purchaseButton.disabled = true;
+        purchaseButton.style.backgroundColor = 'gray';
+    }
+}
 
-  autoClickerCount.textContent = ClickerCount;
+function updateAutoClickerCount()
+autoClickerCount.textContent = ClickerCount;
+
+function updateAutoClickerCost() {
+    autoClickerCost.textContent = clickerCost;
+}
+
+purchaseButton.addEventListener("click", function () {
+  if (donutsCount >= 15) {
+    donutsCount -= 10;
+    ClickerCount++;
+        updateAutoClickerCount();
+        updateButtonState();
+  }
 });
+
+updateButtonState();
+updateAutoClickerCount();
+updateAutoClickerCost();
