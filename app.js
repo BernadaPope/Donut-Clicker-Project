2c7fe61597101553655ed503ba9148b8ddeff50a
BernadaPope <popebernada@.com>
2023-05-12 20:33:03 -0400

    updates

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
-const button = document.getElementById('clickButton');
-const clickCount = document.getElementById('clickCount');
+const button = document.getElementById("clickButton");
+const clickCount = document.getElementById("clickCount");
 
 let counter = 0;
 
-button.addEventListener('click', function () {
-    counter++;
+button.addEventListener("click", function () {
+  counter++;
 
-    clickCount.textContent = 'Click count:' + counter;
-    });
+  clickCount.textContent = "Click count:" + counter;
+});
+const autoClickerSection = document.getElementById("autoClickerSection");
+const autoClickerCount = document.getElementById("autoClickerCount");
+const purchaseButton = document.getElementById("purchaseButton");
+
+let ClickerCount = 0;
+
+purchaseButton.addEventListener("click", function () {
+  ClickerCount++;
+
+  autoClickerCount.textContent = ClickerCount;
+});
