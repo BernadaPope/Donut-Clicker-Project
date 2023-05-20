312a0d6a9e6eca6c5ad53bee17053bd5f57d788f
BernadaPope <popebernada@.com>
2023-05-19 23:09:19 -0400

    fixing buttons

--- app.js
+++ app.js
@@ -8,11 +8,11 @@ button.addEventListener("click", function () {
 
   clickCount.textContent = "Click count:" + counter;
 });
-const autoClickerSection = document.getElementById('autoClickerSection');
-const autoClickerCount = document.getElementById('autoClickerCount');
-const purchaseButton = document.getElementById('purchaseButton');
-const autoClickerCost = document.getElementById('autoClickerCost');
-const resetButton = document.getElementById('resetButton');
+const autoClickerSection = document.getElementById("autoClickerSection");
+const autoClickerCount = document.getElementById("autoClickerCount");
+const purchaseButton = document.getElementById("purchaseButton");
+const autoClickerCost = document.getElementById("autoClickerCost");
+const resetButton = document.getElementById("resetButton");
 
 let ClickerCount = 0;
 let donutsCount = 15;
@@ -21,52 +21,52 @@ let clickerCost = 5;
 function updateButtonState() {
   if (donutsCount >= clickerCost) {
     purchaseButton.disabled = false;
-    purchaseButton.style.backgroundColor = '';
+    purchaseButton.style.backgroundColor = "";
   } else {
-    purchaseButton.disabled = true;
-    purchaseButton.style.backgroundColor = 'gray';
+    purchaseButton.disabled = false;
+    purchaseButton.style.backgroundColor = "gray";
   }
 }
 
 function updateButtonState() {
-    if (donutsCount >= clickerCost) {
-        purchaseButton.disabled = false;
-        purchaseButton.style.backgroundColor = '';
-    } else {
-        purchaseButton.disabled = true;
-        purchaseButton.style.backgroundColor = 'gray';
-    }
+  if (donutsCount >= clickerCost) {
+    purchaseButton.disabled = false;
+    purchaseButton.style.backgroundColor = "";
+  } else {
+    purchaseButton.disabled = false;
+    purchaseButton.style.backgroundColor = "gray";
+  }
 }
 
-function updateAutoClickerCount()
+// function updateAutoClickerCount()
 autoClickerCount.textContent = ClickerCount;
 
 function updateAutoClickerCost() {
-    autoClickerCost.textContent = clickerCost;
+  autoClickerCost.textContent = clickerCost;
 }
 
 function resetGame() {
-    clickerCount = 0;
-    donutsCount = 15;
-    clickerCost = 5;
-    updateAutoClickerCount();
-    updateButtonState();
-    updateAutoClickerCost();
+  ClickerCount = 0;
+  donutsCount = 15;
+  clickerCost = 5;
+  updateAutoClickerCount();
+  updateButtonState();
+  updateAutoClickerCost();
 }
 
 purchaseButton.addEventListener("click", function () {
   if (donutsCount >= clickerCost) {
     donutsCount -= clickerCost;
     ClickerCount++;
-        updateAutoClickerCount();
-        updateButtonState();
+    updateAutoClickerCount();
+    updateButtonState();
   }
 });
 
-resetButton.addEventListener('click', function () {
-    resetGame();
+resetButton.addEventListener("click", function () {
+  resetGame();
 });
 
 updateButtonState();
 updateAutoClickerCount();
-updateAutoClickerCost();
+// updateAutoClickerCost();
