6ea82446d08b6f38bea43db1b0d5eae87c864ade
BernadaPope <popebernada@.com>
2023-06-15 17:52:28 -0400

    updates

--- app.js
+++ app.js
@@ -1,20 +1,47 @@
-const button = document.getElementById("donut count");
-const clickCount = document.getElementById("addDonutButton");
+const donutCountElement = document.getElementById("donut count");
+const autoClickerCountElement = document.getElementById("clickerCount");
+const addDonutButton = document.getElementById("addDonutButton");
+const PurchaseAutoClickerButton = document.getElementById(
+
+);
 
 let counter = 0;
+let AutoClickerCount = 0;
+let addClickerCost = 100;
+let clickCount = 0;
 
 function updateDonutCount() {
   donutCountElement.textContent = donutCount;
-
 }
 
 function addDonut() {
-  donutCount++
+  donutCount++;
   updateDonutCount();
 }
 
-addDonutButton.addEventListener('click', function() {
+function purchaseAutoClicker() {
+  if (donutCount >= 100) {
+    donutCount -= 100;
+    autoClickerCount++;
+    autoClickerCost += Math.round(autoClickerCost * 0.1);
+    updateDonutCount();
+    updateAutoClickerCount();
+  }
+}
+
+addDonutButton.addEventListener("click", function () {
   addDonut();
 });
 
+addPurchaseAutoClickerButton.addEventListener("click", function () {
+  purchaseAutoClicker();
+});
+
+document.addEventListener('click' funtion() {
+  increaseClickCount();
+
+});
+
 updateDonutCount();
+updateAutoClickerCount();
+updateClickCount();
