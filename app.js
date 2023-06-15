eea8c9e6a904e2170d1ff2dc4750e3438636cf62
BernadaPope <popebernada@.com>
2023-06-15 18:27:02 -0400

    updates

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const donutCountElement = document.getElementById("donut count");
-const autoClickerCountElement = document.getElementById("clickerCount");
-const addDonutButton = document.getElementById("addDonutButton");
-const PurchaseAutoClickerButton = document.getElementById(
+const donutCountElement = document.getElementById('donut count');
+const autoClickerCountElement = document.getElementById('clickerCount');
+const addDonutButton = document.getElementById('addDonutButton');
+const purchaseAutoClickerButton = document.getElementById('purchaseAutoClickerButton');
+const activateAutoClickersButton = document.getElementById('activateAutoClicker');
 
-);
 
 let counter = 0;
 let AutoClickerCount = 0;
@@ -19,14 +19,26 @@ function addDonut() {
   updateDonutCount();
 }
 
+function increaseClickCount() {
+  clickCount++;
+  updateClickCount();
+}
+
 function purchaseAutoClicker() {
-  if (donutCount >= 100) {
-    donutCount -= 100;
+  if (clickCount >= addClickerCost && donutCount >= autoClickerCost) {
+    clickCount -= autoClickerCost;
+    donutCount -= autoClickerCost;
     autoClickerCount++;
     autoClickerCost += Math.round(autoClickerCost * 0.1);
     updateDonutCount();
     updateAutoClickerCount();
   }
+}
+
+function activateAutoClickers() {
+  const donutsToAdd = autoClickerCount;
+  donutCount += donutsToAdd;
+  updateDonutCount();
 }
 
 addDonutButton.addEventListener("click", function () {
@@ -37,7 +49,7 @@ addPurchaseAutoClickerButton.addEventListener("click", function () {
   purchaseAutoClicker();
 });
 
-document.addEventListener('click' funtion() {
+document.addEventListener('click' function() {
   increaseClickCount();
 
 });
