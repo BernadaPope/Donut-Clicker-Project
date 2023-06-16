65d761efb300b9a19abe4477fe4af8ba45a08316
BernadaPope <popebernada@.com>
2023-06-16 02:27:11 -0400

    updates

--- app.js
+++ app.js
@@ -20,35 +20,42 @@ class donutMaker {
       this.addDonut();
     }
   }
-    reset() {
-      this.donuts = 0;
-      this.autoClickers = 0;
-      this.autoClickerCost = 100;
-    }
-  }
-  
-  class App {
-    constructor() {
-      this.donutMaker = new donutMaker();
-    }
-
-start() {
-  document.getElementById("Click").addEventListener("click", this.onButtonClick.bind(this));
-  document.getElementById("buy-autoclicker").addEventListener("click", this.onBuyAutoClickerClick.bind(this));
-  document.getElementById("reset").add.addEventListener("click", this.onResetClick.bind(this));
-
-}
-onButtonClick() {
-  this.donutmaker.addDonut();
-  document.getElementById("donuts").innerHTML = this.donutMaker.donuts;
-}
-onBuyAutoClickerClick() {
-  this.donutmaker.purchaseAutoClicker();
-  document.getElementById("autoclclickers").innerHTML = this.donutMaker.autoClickers;
-}
-  
-onResetClick() {
-  this.donutMaker.reset();
-  document.getElementById("autoclickers").innerHTML = this.donutMaker.autoClickers;
+  reset() {
+    this.donuts = 0;
+    this.autoClickers = 0;
+    this.autoClickerCost = 100;
   }
 }
+
+class App {
+  constructor() {
+    this.donutMaker = new donutMaker();
+  }
+
+  start() {
+    document.getElementById("click").addEventListener("click",this.onButtonClick.bind(this));
+    document.getElementById("buy-autoClicker").addEventListener("click",this.onBuyAutoClickerClick.bind(this));
+    document.getElementById("click").addEventListener("click",this.onBuyAutoClickerClick.bind(this));
+    document.getElementById("reset").addEventListener("click", this.onResetClick.bind(this));
+  }
+  onButtonClick() {
+    this.donutMaker.addDonut();
+    document.getElementById("donuts").innerHTML=this.donutMaker.donuts;
+  }
+  
+  onBuyAutoClickerClick() {
+    if (this.donuts >= this.autoClickerCost) {
+      this.donuts -= this.autoClickerCost;
+      this.autoClickers++;
+      this.autoClickerCost*=1.1;
+    }
+    document.getElementById("autoClickers").innerHTML = this.donutMaker.autoClickers;
+  }
+
+  onResetClick() {
+    this.donutMaker.reset();
+    document.getElementById("autoClickers").innerHTML=this.donutMaker.autoClickers;
+  }
+}
+const app = new App();
+app.start();
